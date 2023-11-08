600e2d1ce872b643664066b8e3e31c6e9a4b5da9
edylabanadorrr <abanadoredylbert@gmail.com>
2023-11-08 13:17:00 +0800

    Modifications

--- app/src/main/java/com/batelectwo/BillActivity.java
+++ app/src/main/java/com/batelectwo/BillActivity.java
@@ -58,7 +58,6 @@ public class BillActivity extends AppCompatActivity {
     private Uri uriImage;
     private Bitmap invoiceBitmap;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
